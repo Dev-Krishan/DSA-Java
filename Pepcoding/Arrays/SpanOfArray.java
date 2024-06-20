dc5a6d22f29ce5ddb0174abd500bafe80279e052
Lucifer AI <devkrishan09@gmail.com>
2024-06-20 13:25:58 +0530

    Create SpanOfArray.java
    Crated Array and added one question

---
+++ Pepcoding/Arrays/SpanOfArray.java
@@ -0,0 +1,36 @@
+/*
+ * SPAN OF ARRAY
+ * We need to find the difference in min and max 
+ * of array of n size
+ */
+
+import java.util.Scanner;
+
+public class SpanOfArray{
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Enter the number of elements");
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = scn.nextInt();
+        }
+
+        helper(arr);
+    }
+
+    static void helper(int[] arr){
+        int max = arr[0];
+        int min = arr[0];
+        for(int i=1; i<arr.length;i++){
+            if(arr[i]>max){
+                max = arr[i];
+            }
+            if(arr[i]<min){
+                min =arr[i]; 
+            }
+        }
+        System.out.println(max-min);
+    }
+}
